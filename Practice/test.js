4b73ba22ea819c9c11d7629588e8ecd298de13b6
shauravbaramu <sauravmagar2000@gmail.com>
2025-02-06 08:27:24 -0500

    Changes added

--- Practice/test.js
+++ Practice/test.js
@@ -1,7 +1,34 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
+// Create an HTTP server
 http.createServer(function (req, res) {
-    res.write('Hellow World');
-    res.end();
+    // Check if the request URL is /index.html
+    if (req.url === '/indexs.html') {
+        // Read the index.html file
+        var filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, function(err, data) {
+            if (err) {
+                // If there is an error reading the file, send a 500 response
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.write('Internal Server Error');
+                res.end();
+            } else {
+                // If the file is read successfully, send the contents as the response
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            }
+        });
+    } else {
+        // If the request is not for /index.html, send a 404 response
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('The page you requested is not available');
+        res.end();
+    }
 
-}).listen(5000);
+// The server listens on port 8080 for incoming connections
+}).listen(5000, function() {
+    console.log('Server is listening on port 5000');
+});
